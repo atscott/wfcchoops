09b5250b87404bf2062ed02b1006c6c232ec2a7e
atscott <atscott@google.com>
2016-08-28 23:01:59 -0700

    play with dependencies. remove promise route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
-import { Routes, RouterModule } from '@angular/router';
-import { Home } from './home';
-import { About } from './about';
-import { NoContent } from './no-content';
+import {RouterModule, Routes} from '@angular/router';
 
-import { DataResolver } from './app.resolver';
+import {About} from './about';
+import {DataResolver} from './app.resolver';
+import {Home} from './home';
+import {NoContent} from './no-content';
 
 
 export const ROUTES: Routes = [
-  { path: '',      component: Home },
-  { path: 'home',  component: Home },
-  { path: 'about', component: About },
-  {
-    path: 'detail', loadChildren: () => System.import('./+detail')
-  },
-  { path: '**',    component: NoContent },
+  {path: '', component: Home},
+  {path: 'home', component: Home},
+  {path: 'about', component: About},
+  {path: '**', component: NoContent},
 ];
