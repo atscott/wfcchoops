4bad0788767946f7143fdc69be7a9faf51b344b4
Andrew Scott <atscott@atscott-macbookpro2.roam.corp.google.com>
2016-09-02 12:12:08 -0700

    fix tab widths
    small adjustment to percent
    
    fix tab widths

--- src/app/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments.component.ts
@@ -1,8 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'tournaments',
-  styles: [],
+  encapsulation: ViewEncapsulation.None,
+  styles: [`
+    tournaments md-tab-group .md-tab-label[class] {
+      min-width: 33.34%;
+    }
+  `],
   templateUrl: './tournaments.template.html'
 })
 export class Tournaments {
