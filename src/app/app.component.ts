072501174feb4ef4ac8ba3ddf83783be97daa815
Andrew Scott <atscott@atscott-macbookpro2.roam.corp.google.com>
2016-09-10 13:50:43 -0700

    more styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,11 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-import {Router} from '@angular/router';
 import {ROUTES} from './app.routes.ts';
 
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styles: [require('./app.style.css')],
-  template: `
-  <md-sidenav-layout fullscreen>
-    <md-sidenav #sidenav [mode]="sidenavMode" [opened]="sidenavMode === 'side'" (window:resize)="onResize($event.target.innerWidth)">
-      <img src="assets/img/tigercropped.jpg">
-      <md-nav-list>
-        <a md-list-item *ngFor="let route of routes" routerLink="{{route.path}}" routerLinkActive="active" (click)="sidenavMode === 'over' && sidenav.close()">
-          <img md-list-icon [src]="route?.data.icon">
-          <span md-line> {{route?.data['title']}} </span>
-        </a>
-      </md-nav-list>
-    </md-sidenav>
-    <md-content>
-      <md-toolbar color="primary">
-        <button disableRipple md-icon-button (click)="sidenav.toggle()" *ngIf="sidenavMode === 'over'">
-          <md-icon>menu</md-icon>
-        </button>
-        <h2>Wrightstown Full Court Club</h2>
-      </md-toolbar>
-
-      <md-card class="middle-column">
-        <router-outlet></router-outlet>
-        <md-divider></md-divider>
-        <div flex layout-align="center center" layout="column">
-          <img src="assets/img/FullCourtLogo-216.png">
-        </div>
-      </md-card>
-
-    </md-content>
-  </md-sidenav-layout>
-  `
+  templateUrl: './app.template.html'
 })
 export class App {
   routes = [];
@@ -50,6 +20,8 @@ export class App {
   }
 
   onResize(width) {
-    this.sidenavMode = width < 700 ? 'over' : 'side';
+    const contentWidth = 700;
+    const sidenavWidth = 476;
+    this.sidenavMode = width < contentWidth + sidenavWidth ? 'over' : 'side';
   }
 }
