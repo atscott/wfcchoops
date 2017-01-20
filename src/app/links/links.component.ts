a07c9b70de3c76fd21ddd3d3b52d6f6249274bda
atscott <atscott01@gmail.com>
2017-01-19 18:02:15 -0800

    clean up old content

--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'links',
   styles: [` `],
-  template: require('./links.template.html')
+  templateUrl: './links.template.html'
 })
 export class Links {
   constructor() {
--- src/app/opengym/opengym.component.ts
+++ src/app/opengym/opengym.component.ts
@@ -3,13 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'open-gym',
   styles: [``],
-  template: `
-<div class="main">
-  <h2>WFCC Open-Gym Schedule</h2>
-  There are no open gyms scheduled for this time.
-  Having trouble viewing? Download this pdf <a href="http://wfcchoops.com/assets/docs/Open Gyms Fall.pdf">here</a>
-  <iframe src="http://docs.google.com/gview?url=http://wfcchoops.com/assets/docs/Open Gyms Fall.pdf&amp;embedded=true" style="width: 100%; height: 800px;" frameborder="0"></iframe><br>
-</div>`
+  templateUrl: './opengym.template.html'
 })
 export class OpenGym {
   constructor() {
