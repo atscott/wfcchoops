a441e98f506f08f293f0cda560b15bfe0f88be9b
atscott <atscott01@gmail.com>
2017-03-21 16:41:41 -0700

    move e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,32 @@
-import { WfcchoopsNgcliPage } from './app.po';
-
-describe('wfcchoops-ngcli App', function() {
-  let page: WfcchoopsNgcliPage;
+describe('App', () => {
 
   beforeEach(() => {
-    page = new WfcchoopsNgcliPage();
+    browser.get('/');
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+
+  it('should have a title', () => {
+    let subject = browser.getTitle();
+    let result = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
+    expect(subject).toEqual(result);
   });
+
+  it('should have header', () => {
+    let subject = element(by.css('h1')).isPresent();
+    let result = true;
+    expect(subject).toEqual(result);
+  });
+
+  it('should have <home>', () => {
+    let subject = element(by.css('app home')).isPresent();
+    let result = true;
+    expect(subject).toEqual(result);
+  });
+
+  it('should have buttons', () => {
+    let subject = element(by.css('button')).getText();
+    let result = 'Submit Value';
+    expect(subject).toEqual(result);
+  });
+
 });
