a83cc3a269999a0bcdb109c61a697174a0f2f73d
Andrew Scott <atscott01@gmail.com>
2018-03-08 10:59:47 -0800

    updates - March 2018

--- src/app/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'tournaments',
-  styles: [],
+  styleUrls: ['./tournaments.style.css'],
   templateUrl: './tournaments.template.html'
 })
 export class Tournaments {
