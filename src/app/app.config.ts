5c2720fca64b3d6b8046158d985eb85a0a8afb92
Andrew Scott <atscott01@gmail.com>
2025-09-06 14:25:44 -0700

    incremental hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {Injectable, provideZonelessChangeDetection} from '@angular/core';
-import {Title, provideClientHydration} from '@angular/platform-browser';
+import {Title, provideClientHydration, withIncrementalHydration} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter, RouterStateSnapshot, TitleStrategy, withViewTransitions} from '@angular/router';
 
@@ -26,7 +26,7 @@ export const appConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(ROUTES, withViewTransitions()),
     {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
-    provideClientHydration(),
+    provideClientHydration(withIncrementalHydration()),
     provideZonelessChangeDetection(),
   ]
 }
