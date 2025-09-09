99c1f4756276799ddec91f3042e310fc67c65238
Andrew Scott <atscott01@gmail.com>
2025-09-08 18:38:59 -0700

    Revert "remove mat sidenav, better support for ssr with css nav"
    This reverts commit 1a89cf2fc5a48c657b3cc2ecb03bc53e48394940.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,53 @@
-import { NgOptimizedImage } from "@angular/common";
-import { Component, OnDestroy, OnInit, signal } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatLineModule } from "@angular/material/core";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatIconModule } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import {NgOptimizedImage} from '@angular/common';
+import {Component, afterNextRender} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatLineModule} from '@angular/material/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterLink, RouterLinkActive, RouterOutlet, Routes} from '@angular/router';
 
-import { ROUTES } from "./app.routes";
+import {ROUTES} from './app.routes';
 
 @Component({
-  selector: "app",
-  styleUrls: ["./app.style.scss"],
-  templateUrl: "./app.template.html",
-  imports: [
-    MatListModule,
-    RouterLinkActive,
-    RouterLink,
-    MatLineModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    RouterOutlet,
-    MatDividerModule,
-    NgOptimizedImage,
-  ],
+    selector: 'app',
+    styleUrls: ['./app.style.scss'],
+    templateUrl: './app.template.html',
+    imports: [
+        MatSidenavModule,
+        MatListModule,
+        RouterLinkActive,
+        RouterLink,
+        MatLineModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        RouterOutlet,
+        MatDividerModule,
+        NgOptimizedImage,
+    ]
 })
 export class AppComponent {
-  routes = ROUTES.filter((r) => r.title);
-  isSidenavOpen = signal(false);
+  routes: Routes = [];
+  sidenavMode: 'side'|'over' = 'side';
 
   constructor() {
-    if (typeof document !== "undefined")
-      window.addEventListener("keydown", (event: KeyboardEvent) => {
-        if (event.key === "Escape") {
-          this.isSidenavOpen.set(false);
-        }
-      });
+    afterNextRender(() => {
+      this.onResize(window.innerWidth);
+    });
   }
 
-  toggleSidenav() {
-    this.isSidenavOpen.update((open) => !open);
+  ngOnInit() {
+    this.routes = ROUTES.filter(r => r.title);
+  }
+
+  onResize(width: number) {
+    const contentWidth = 700;
+    const sidenavWidth = 476;
+    this.sidenavMode = width < contentWidth + sidenavWidth ? 'over' : 'side';
   }
 }
