28c51cc386f062c50868ab02f80184319a041060
Andrew Scott <atscott01@gmail.com>
2026-09-16 17:56:10 -0700

    chore: update 2026 Young Tigers program
    - Update brochure and program details for 2026
    - Update dev server configuration to port 3000
    - Clean up unused project files

--- server.ts
+++ server.ts
@@ -51,11 +51,11 @@ export function app(): express.Express {
 }
 
 function run(): void {
-	const port = process.env['PORT'] || 4000;
+	const port = process.env['PORT'] || 3000;
 
 	// Start up the Node server
 	const server = app();
-	server.listen(port, () => {
+	server.listen(Number(port), '0.0.0.0', () => {
 		console.log(`Node Express server listening on http://localhost:${port}`);
 	});
 }
